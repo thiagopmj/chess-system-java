39d6ff9b3336657300166a94a3182017857eaab2
thiagopmj <thiago@jau.sp.gov.br>
2020-05-11 16:46:07 -0300

    Chess layer and printing the board

---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece {
+	
+	private Color clor;
+
+	public ChessPiece(Board board, Color clor) {
+		super(board);
+		this.clor = clor;
+	}
+
+	public Color getClor() {
+		return clor;
+	}
+	
+	
+}
